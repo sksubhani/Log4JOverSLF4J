976ce30b1efc0e9182123ac3b165d562cf65b2ad
Subhani Shaik <subhani.shaik@disney.com>
2016-08-31 15:40:11 -0700

    Initial code

---
+++ src/main/java/log4jdemooverslf4j/Log4JOverSLF4JDemo.java
@@ -0,0 +1,17 @@
+package log4jdemooverslf4j;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Log4JOverSLF4JDemo {
+
+	private static final Logger logger = LoggerFactory.getLogger(Log4JOverSLF4JDemo.class);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		logger.debug("Hello");
+		
+	}
+
+}
